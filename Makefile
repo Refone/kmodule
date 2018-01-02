de23ac130e779d801c7d4a1f525d3c54bb66fe70
Liu Ruifeng <710196926@qq.com>
2018-01-02 15:57:21 +0800

    Changes to be committed: 	modified:   Makefile 	modified:   main.c

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-obj-m = main.o
+obj-m = module.o
+module-objs := main.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- main.c
+++ main.c
@@ -2,30 +2,17 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 
-static int whatever;
-static int answer = 42;
-static char __initdata howdymsg[] = "Good day, eh?";
-static char __exitdata exitmsg[] = "Taking off, eh?";
-
-void useless(void)
-{
-    printk(KERN_INFO "I am totally useless.\n");
-}
-
 static int __init hi(void)
 {
     printk(KERN_INFO "module m1 being loaded.\n");
-    printk(KERN_INFO "%s\n", howdymsg);
-    printk(KERN_INFO "The answer is %d.\n", answer);
-    answer = 999;
+
+
     return 0;
 }
 
 static void __exit bye(void)
 {
     printk(KERN_INFO "module m1 being unloaded.\n");
-    printk(KERN_INFO "%s\n", exitmsg);
-    printk(KERN_INFO "The answer is now %d.\n", answer);
 }
 
 module_init(hi);
